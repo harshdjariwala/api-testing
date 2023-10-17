4884ca2c96a56df858dd81db895b8d216d8a1fcc
Harsh <harshjariwala81@gmail.com>
2023-10-17 17:06:38 +0530

    Fixed sql.close()

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
 const sql = require('mssql');
 const express = require('express');
-// uspGetAssignedMenus
-const app = express();
-const port = 3000; // Choose a port for your application
+const dotenv = require('dotenv');
+const http = require('http');
 
-const config = {
-  user: 'sa',
-  password: 'bank@Yb3S9',
-  server: '103.133.122.173',
-  database: 'ERPUserDb',
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const dbConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
   options: {
     encrypt: true,
     trustServerCertificate: true,
   },
 };
 
-const key = '1234567890123456'; // Replace with your actual API key
+const apiKey = process.env.API_KEY;
+
+// Create a connection pool
+const pool = new sql.ConnectionPool(dbConfig);
+const poolConnect = pool.connect();
 
 async function connectToDatabase() {
   try {
-    await sql.connect(config);
+    await poolConnect;
     console.log('Connected to the database');
   } catch (err) {
     console.error('Error connecting to the database:', err);
@@ -28,74 +36,45 @@ async function connectToDatabase() {
 
 // Middleware to check the API key
 function apiKeyMiddleware(req, res, next) {
-  const apiKey = req.query.key;
+  const requestApiKey = req.query.key;
 
-  if (!apiKey || apiKey !== key) {
+  if (!requestApiKey || requestApiKey !== apiKey) {
     return res.status(401).send('Unauthorized. Invalid API key.');
   }
 
-  next(); // Move on to the next middleware or route handler if the API key is valid
+  next();
 }
 
 app.use(apiKeyMiddleware);
 
-app.get('/getAssignedMenus', async (req, res) => {
-  const iMenuId = req.query.iMenuId;
-  const iGroupId = req.query.iGroupId;
-
-  if (!iMenuId && !iGroupId) {
-    return res.status(400).send('Please provide a valid iRoleUserId parameter.');
-  }
-
+app.get('/getMenuHeaders', async (req, res) => {
   try {
-    const request = new sql.Request();
-    request.input('iMenuId', sql.Int, iMenuId);
-    request.input('iGroupId', sql.Int, iGroupId); // Assuming iRoleUserId is an integer, adjust the type accordingly
-    const result = await request.query('EXEC uspGetAssignedMenus @iMenuId, @iGroupId');
+    const request = pool.request();
+    const result = await request.execute('uspGetMenuHeaders');
     res.json(result.recordset);
   } catch (err) {
     console.error('Error executing SQL query:', err);
     res.status(500).send('Error executing SQL query.');
-  } finally {
-    sql.close(); // Close the database connection after the query is executed
   }
 });
 
-app.get('/getMenuHeaders', async (req, res) => {
-    try {
-      const request = new sql.Request();
-      const result = await request.query('EXEC uspGetMenuHeaders');
-      res.json(result.recordset);
-    } catch (err) {
-      console.error('Error executing SQL query:', err);
-      res.status(500).send('Error executing SQL query.');
-    } finally {
-      sql.close(); // Close the database connection after the query is executed
-    }
-  });
+const server = http.createServer(app);
 
-app.get('/getUserRoles', async (req, res) => {
-    const iRoleUserId = req.query.iRoleUserId;
-  
-    if (!iRoleUserId) {
-      return res.status(400).send('Please provide a valid iRoleUserId parameter.');
-    }
-  
-    try {
-      const request = new sql.Request();
-      request.input('iRoleUserId', sql.Int, iRoleUserId); // Assuming iRoleUserId is an integer, adjust the type accordingly
-      const result = await request.query('EXEC uspGetUserRoles @iRoleUserId');
-      res.json(result.recordset);
-    } catch (err) {
-      console.error('Error executing SQL query:', err);
-      res.status(500).send('Error executing SQL query.');
-    } finally {
-      sql.close(); // Close the database connection after the query is executed
-    }
-});
-
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
   connectToDatabase();
 });
+
+// Gracefully close the connection pool when the server is stopped
+const gracefulShutdown = () => {
+  server.close(() => {
+    console.log('Server has been closed');
+    pool.close().then(() => {
+      console.log('Database connection pool has been closed');
+      process.exit(0);
+    });
+  });
+};
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
